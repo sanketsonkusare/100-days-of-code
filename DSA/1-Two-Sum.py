bec0ae915514f04044daf67503c4188bd0bc8a73
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-09-23 22:49:33 +0530

    Refactor twoSum function to use a hash map for improved performance

--- DSA/1-Two-Sum.py
+++ DSA/1-Two-Sum.py
@@ -1,12 +1,25 @@
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#         return[]
+    
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        hash = {}
+        for i, num in enumerate(nums):
+            comp = target - num
+
+            if comp in hash:
+                return [hash[comp], i]
+        
+            hash[num] = i
+        
         return[]
