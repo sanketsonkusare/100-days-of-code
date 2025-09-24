742efed86366997c545dd2411a6880315ba09280
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-09-24 22:54:17 +0530

    solved using two pointer apprach

--- DSA/2-reverse-string.py
+++ DSA/2-reverse-string.py
@@ -4,7 +4,17 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        a = s[:]
-        length = len(s)
-        for i in range(length):
-            s[i] = a[length-1-i]
+        """approach 1"""
+        # a = s[:]
+        # length = len(s)
+        # for i in range(length):
+        #     s[i] = a[length-1-i]
+
+        """approach 2"""
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
