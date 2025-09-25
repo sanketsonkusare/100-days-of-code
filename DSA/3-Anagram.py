f98e95b33ff923fda3233f94a95bedc176f537a3
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-09-25 23:40:49 +0530

    Refactor isAnagram method to improve efficiency by checking length and character counts

--- DSA/3-Anagram.py
+++ DSA/3-Anagram.py
@@ -12,4 +12,15 @@ class Solution(object):
         # else:
         #     return False
 
-        return sorted(s) == sorted(t)
+
+        # return sorted(s) == sorted(t)
+
+
+        # more efficient solution coz it checks for all the false case first that is much faster then it searches wather there exists another false case or not if not then it returns true
+        
+        if len(s)!=len(t):
+            return False
+        for index in set(s):
+            if s.count(index)!=t.count(index):
+                return False
+        return True
