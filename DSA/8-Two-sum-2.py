2450a8cbdd993febf6c846f548461ac576f3c0b2
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-09-30 23:23:09 +0530

    Two pointer approach

--- DSA/8-Two-sum-2.py
+++ DSA/8-Two-sum-2.py
@@ -5,7 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(numbers)-1):
-            for j in range(1+i, len(numbers)):
-                if numbers[i] + numbers[j] == target:
-                    return i+1,j+1
+        # for i in range(len(numbers)-1):
+        #     for j in range(1+i, len(numbers)):
+        #         if numbers[i] + numbers[j] == target:
+        #             return i+1,j+1
+
+        start = 0
+        end = len(numbers)-1
+
+        while start < end:
+            if numbers[start]+numbers[end] > target:
+                end -= 1
+
+            elif numbers[start]+numbers[end] < target:
+                start +=1
+
+            else:
+                return [start+1, end+1]
