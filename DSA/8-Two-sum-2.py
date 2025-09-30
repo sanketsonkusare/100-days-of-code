34a5a70843ac80b756189b7ca97b8aeaf1856717
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-09-30 23:39:13 +0530

    most optimised solution using hashmap

--- DSA/8-Two-sum-2.py
+++ DSA/8-Two-sum-2.py
@@ -10,15 +10,23 @@ class Solution(object):
         #         if numbers[i] + numbers[j] == target:
         #             return i+1,j+1
 
-        start = 0
-        end = len(numbers)-1
+        # start = 0
+        # end = len(numbers)-1
 
-        while start < end:
-            if numbers[start]+numbers[end] > target:
-                end -= 1
+        # while start < end:
+        #     if numbers[start]+numbers[end] > target:
+        #         end -= 1
 
-            elif numbers[start]+numbers[end] < target:
-                start +=1
+        #     elif numbers[start]+numbers[end] < target:
+        #         start +=1
 
-            else:
-                return [start+1, end+1]
+        #     else:
+        #         return [start+1, end+1]
+
+        hashmap = defaultdict(list)
+        i = 0
+        for num in numbers:
+            if target-num in hashmap:
+                return [hashmap[target-num] + 1, i + 1]
+            hashmap[num] = i
+            i += 1
