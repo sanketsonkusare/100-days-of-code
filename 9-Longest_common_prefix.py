650790efabf05532b52f10e9d20d7e513b899c8c
sanketsonkusare <sanketsonkusare01@gmail.com>
2025-10-22 18:51:23 +0530

    Optimised solution

--- 9-Longest_common_prefix.py
+++ 9-Longest_common_prefix.py
@@ -1,4 +1,17 @@
 # Brute-Force
+'''class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs: return ""
+
+        for i, char in enumerate(strs[0]):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != char:
+                    return strs[0][:i]
+
+        return strs[0]'''
+    
+# Optimised-Solution
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs: return ""
@@ -9,4 +22,3 @@ class Solution:
                     return strs[0][:i]
 
         return strs[0]
-    
